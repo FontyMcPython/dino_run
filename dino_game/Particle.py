01776648759c1324a4f0dafeb31840d114c34f2e
Fonty McPython <joseantoniofontcia@gmail.com>
2019-03-07 20:31:12 +0100

    ciempieses v1

--- dino_game/Particle.py
+++ dino_game/Particle.py
@@ -2,9 +2,30 @@ class Particle(object):
     def __init__(self):
         self.ypos = 450 + random(300)
         self.xpos = width
-        
+        self.gusano = 0
+        if random(100) <= 1:
+            self.anim = 0
+            self.gusano = 1
+            
     def plot(self):
-        line(self.xpos, self.ypos, self.xpos + 10, self.ypos)
+        if self.gusano == 1:
+            if self.anim >= 0 and self.anim <= 5:
+                img1 = loadImage("pixil-frame-0.png")
+                image(img1, self.xpos, self.ypos);
+            elif self.anim > 5:
+                img1 = loadImage("pixil-frame-1.png")
+                image(img1, self.xpos, self.ypos);
+            elif self.anim >= -5 and self.anim < 0:
+                img1 = loadImage("pixil-frame-2.png")
+                image(img1, self.xpos, self.ypos);
+            else:
+                img1 = loadImage("pixil-frame-0.png")
+                image(img1, self.xpos, self.ypos);
+            self.anim += 3
+            if self.anim > 10:
+                self.anim = -10
+        else:
+            line(self.xpos, self.ypos, self.xpos + 10, self.ypos)
     
     def move(self):
         if self.xpos > 1:
